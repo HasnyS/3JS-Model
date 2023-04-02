a71b5b4ccf0d322a9602cb9f2b71de9ff2c034a6
Sama <101380876+HasnyS@users.noreply.github.com>
2023-04-01 21:44:58 -0400

    v2-> navbar done hero in progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {motion} from "framer-motion";
+import {styles} from "../style.js";
+import {ComputersCanvas} from "./canvas";
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+      //background
+      <section className={'relative w-full h-screen mx-auto'}>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max w-7xl mx-auto flex flex-row items-start gap-5`}>
+            <div className='flex flex-col justify-center items-center mt-5'>
+                {/*circle */}
+                <div className={'w-5 h-5 rounded-full bg-[#915eff]'}/>
+                {/*line */}
+                <div className={'w-1 sm:h-80 h-40 violet-gradient'}/>
+                {/* Title */}
+                <div>
+                    <h1 className={`${styles.heroHeadText} text-white`}>
+                        Hi, I'm <span className={'text-[#915eff]'}>
+                        Syed
+                    </span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+      </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,48 @@
 import React, {useEffect, useState} from 'react'
 import {Link} from "react-router-dom";
 import {styles} from "../style.js";
-import {navLinks} from "../constants/index.js";
+import {navLinks} from "../constants/";
 import {logo, menu, close} from '../assets';
 
 const Navbar = () => {
-  const [active,setActive] =useState('');
+  const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
 
   return (
       <nav
-          className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}>
-
-        <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+          className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 'bg-primary`}
+      >
+          <div
+            className="w-full flex justify-between items-center max-w-7xl mx-auto"
+          >
           <Link
-              to="/"
-              className="flex items-center gap-2"
+              to='/'
+              className='flex items-center gap-2'
               onClick={() => {
                 setActive("");
                 window.scrollTo(0, 0)
-              }}>
+              }}
+          >
             <img src={logo} alt ="logo" className={"w-9 h-9 object-contain"}/>
-            <p className={"text-white text-[18px] font-bold cursor-pointer"}> Hasny</p>
+            <p className={'text-white text-[18px] font-bold cursor-pointer flex '}>
+                Hasny
+            </p>
           </Link>
-            <ul className={'list-none hidden sm:flex flex-row gap-10'}>
-                {navLinks.map((link) => (
-                    <li
-                        key={link.id}
-                        className={`${
-                            active === link.title
-                            ? "text-white"
-                                : "text-secondary"
-                        } hover:text-white text-[18px]
-                        font-medium cursor-pointer`}
-                        onClick={ () => setActive(link.title)}
-                    >
-                        <a href={`#${link.id}`} >
-                        {link.title}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            <div
-            className={"sm-hidden flex flex-1 justify-end items-center"}>
+              <ul className='list-none hidden sm:flex flex-row gap-10'>
+                  {navLinks.map((nav) => (
+                      <li
+                          key={nav.id}
+                          className={`${
+                              active === nav.title ? "text-white" : "text-secondary"
+                          } hover:text-white text-[18px] font-medium cursor-pointer`}
+                          onClick={() => setActive(nav.title)}
+                      >
+                          <a href={`#${nav.id}`}>{nav.title}</a>
+                      </li>
+                  ))}
+              </ul>
+
+              <div className={'sm:hidden flex flex-1 justify-end items-center'}>
                 <img
                     src={toggle ? close : menu}
                     alt={menu}
@@ -66,7 +66,6 @@ const Navbar = () => {
                                     {link.title}
                                 </a>
                             </li>
-
                         ))}
                     </ul>
                 </div>
