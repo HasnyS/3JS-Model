ade3bd0741c0dfa328ece2ba7e6c294dc92a1a41
Sama <101380876+HasnyS@users.noreply.github.com>
2023-04-02 15:21:14 -0400

    v2-> 3d model initial upload and base hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ import {ComputersCanvas} from "./canvas";
 const Hero = () => {
   return (
       //background
-      <section className={'relative w-full h-screen mx-auto'}>
-        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <section className={`relative w-full h-screen mx-auto`}>
+        <div className={`absolute inset-0 top-[120px] max w-7xl mx-auto ${styles.paddingX}  flex flex-row items-start gap-5`}>
             <div className='flex flex-col justify-center items-center mt-5'>
                 {/*circle */}
                 <div className={'w-5 h-5 rounded-full bg-[#915eff]'}/>
@@ -20,9 +20,14 @@ const Hero = () => {
                         Syed
                     </span>
                     </h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        descripton <br className='sm:block hidden' />
+                        description                    </p>
                 </div>
             </div>
         </div>
+          <ComputersCanvas />
+
       </section>
   )
 }
